ddfac1662fb6c4ebb9bec0d7575516a44d9bd17d
Vishnu Madhusoodanan <vishnu.madhusoodanan@coinbase.com>
2024-08-06 16:43:29 -0500

    1. Polyfill

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// 1. Polyfill
+import "./src/polyfill";
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
