bb9f628af51f94ddfe65eb3590893c5f236adb4d
Vishnu Madhusoodanan <vishnu.madhusoodanan@coinbase.com>
2024-08-06 16:53:46 -0500

    2. Setup deeplinking

--- App.tsx
+++ App.tsx
@@ -3,8 +3,27 @@ import "./src/polyfill";
 
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import * as Linking from "expo-linking";
+import { useEffect } from "react";
+
+// exp://x.x.x.x:8000/--/
+const PREFIX_URL = Linking.createURL("/");
 
 export default function App() {
+  // 2. Setup deeplinking
+  useEffect(() => {
+    const subscription = Linking.addEventListener("url", ({ url }) => {
+      console.log("incoming deeplink:", url);
+      try {
+        // handleResponse(url);
+      } catch (err) {
+        console.error(err);
+      }
+    });
+
+    return () => subscription.remove();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
